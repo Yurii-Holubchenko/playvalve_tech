2c236027dd504af2f5a85070e6592743ee0edca8
Yurii Holubchenko <yurii.holubchenko@gmail.com>
2024-03-29 14:17:47 +0100

    Added methods, validations and default value for ban_status field. Added validations for idfa.

--- app/models/concerns/ban_status.rb
+++ app/models/concerns/ban_status.rb
@@ -1,5 +1,19 @@
 module BanStatus
-  BAN_STATUS = %w(banned not_banned)
+  extend ActiveSupport::Concern
 
-  validates :ban_status, inclusion: { in: BAN_STATUS }
+  BAN_STATUS = [
+    BANNED = "banned",
+    NOT_BANNED = "not_banned"
+  ].freeze
+
+  included do
+    attribute :ban_status, default: -> { NOT_BANNED }
+    validates :ban_status, inclusion: { in: BAN_STATUS }
+  end
+
+  BAN_STATUS.each do |status_name|
+    define_method("#{status_name}?") do
+      ban_status == status_name
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   include BanStatus
+
+  validates :idfa, presence: true, uniqueness: true
 end

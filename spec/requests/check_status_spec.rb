6e345ce2f3858c7f7b7d1e8408b825e40fb4b3eb
Yurii Holubchenko <yurii.holubchenko@gmail.com>
2024-03-29 16:55:25 +0100

    Added functionality for controller status check.

--- spec/requests/check_status_spec.rb
+++ spec/requests/check_status_spec.rb
@@ -1,7 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "CheckStatuses", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+describe "CheckStatusController" do
+  context "#create" do
+    let(:params) do
+      {
+        idfa: "111",
+        rooted_device: false
+      }
+    end
+
+    it "creates new user" do
+      post "/v1/users/check_status", params: params, headers: headers
+
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq({ban_status: User::NOT_BANNED}.to_json)
+    end
   end
 end
